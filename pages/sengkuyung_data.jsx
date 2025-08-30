d19bb5d1f43234f8c6d7bc09d9b97e061f4a40ef
rznive <rezaofc.jr@gmail.com>
2025-08-30 08:55:28 +0700

    change index array summarydata and command pnbp card

--- pages/sengkuyung_data.jsx
+++ pages/sengkuyung_data.jsx
@@ -107,7 +107,7 @@ export default function MenuSengkuyung() {
                     )}
                   </div>
 
-                  {/* PNBP */}
+                  {/* PNBP
                   <div className="p-4">
                     <div className="text-sm font-medium text-gray-600">
                       PNBP*
@@ -119,7 +119,7 @@ export default function MenuSengkuyung() {
                         Rp{table2?.summary?.total?.[13]}
                       </div>
                     )}
-                  </div>
+                  </div> */}
                 </div>
               </CardContent>
             </Card>
@@ -147,7 +147,7 @@ export default function MenuSengkuyung() {
                     ) : (
                       <>
                         <div className="text-2xl font-bold text-green-700">
-                          {table2?.summary?.total?.[1]}
+                          {table2?.summary?.total?.[2]}
                         </div>
                         <div className="mt-2">
                           <p className="text-sm font-medium text-gray-600">
@@ -233,7 +233,7 @@ export default function MenuSengkuyung() {
                     )}
                   </div>
 
-                  {/* PNBP */}
+                  {/* PNBP
                   <div className="p-4">
                     <div className="text-sm font-medium text-gray-600">
                       PNBP*
@@ -255,7 +255,7 @@ export default function MenuSengkuyung() {
                         </div>
                       </>
                     )}
-                  </div>
+                  </div> */}
                 </div>
               </CardContent>
             </Card>
